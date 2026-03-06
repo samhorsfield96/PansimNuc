c391edf56569b83b5af7a70c11ad04ecaf56e516
Samuel Horsfield <samuel.horsfield@unine.ch>
2026-03-06 15:03:18 +0100

    Adds reading of config files

--- pansimnuc/src/main.rs
+++ pansimnuc/src/main.rs
@@ -1,7 +1,9 @@
 mod gff;
+mod config;
 
 use clap::Parser;
 use gff::read_gff_lines;
+use config::Config;
 
 #[derive(Parser, Debug)]
 #[command(name = "pansimnuc")]
@@ -12,14 +14,38 @@ struct Args {
 
 	#[arg(long, help = "Path to FASTA file")]
 	fasta: String,
+
+	#[arg(long, help = "Optional path to config file")]
+	config: Option<String>,
 }
 
 fn main() {
 	let args = Args::parse();
 
+	// Load config if provided
+	if let Some(config_path) = &args.config {
+		match Config::from_file(config_path) {
+			Ok(config) => {
+				println!("Loaded config from: {}", config_path);
+				for section in config.sections.keys() {
+					println!("  [{}]", section);
+					for key in config.keys_in_section(section) {
+						if let Some(value) = config.get(section, &key) {
+							println!("    {} = {}", key, value);
+						}
+					}
+				}
+			}
+			Err(err) => {
+				eprintln!("Failed to read config file: {err}");
+				std::process::exit(1);
+			}
+		}
+	}
+
 	match read_gff_lines(&args.gff, &args.fasta) {
 		Ok(features) => {
-			println!("Loaded {} features", features.len());
+			println!("Loaded {} contigs with features", features.len());
 		}
 		Err(err) => {
 			eprintln!("Failed to read input files: {err}");
