22e0b198bb98cbe988cef4f46dac648aca855867
Samuel Horsfield <samuel.horsfield@unine.ch>
2026-03-09 10:18:46 +0100

    Adds double exponential distribution

--- pansimnuc/src/mutation.rs
+++ pansimnuc/src/mutation.rs
@@ -15,14 +15,54 @@
 
 use rustc_hash::FxHashMap;
 use rand::Rng;
-use rand_distr::{Distribution as _, Normal, Uniform, Exponential, Laplace};
+use rand_distr::Normal;
+use rand::distr::Uniform;
+use statrs::distribution::Exp;
+use rand::rngs::StdRng;
+
+struct DoubleExponential {
+    lambda1: f64,
+    lambda2: f64,
+    prop_lambda1: f64,
+    cutoff: f64,
+    sample: f64
+}
+
+impl DoubleExponential {
+    pub fn new(lambda1: f64, lambda2: f64, prop_lambda1: f64, cutoff: f64, rng: &mut StdRng) -> Self {
+        let uniform: Uniform<f64> = Uniform::new(0.0, 1.0).unwrap();
+        let exponential1: Exp = Exp::new(lambda1).unwrap();
+        let exponential2: Exp = Exp::new(lambda2).unwrap();
+
+        let weight: f64 = uniform.sample(&mut rng) as f64;
+        let mut sample: f64 = 0.0;
+
+        // higher selected gene
+        if weight <= cutoff {
+            sample = exponential2.sample(&mut rng);
+            //selection_coeffient = 100.0;
+        } else {
+            sample = exponential1.sample(&mut rng);
+
+            while sample > 1.0 {
+                sample = exponential1.sample(&mut rng);
+            }
+
+            sample = -1.0 * sample;
+        }
+
+        Self { lambda1, lambda2, prop_lambda1, cutoff, sample }
+    }
+    
+
+}
 
 #[derive(Clone)]
 pub enum Distribution {
     Normal { mean: f64, std_dev: f64 },
     Uniform { low: f64, high: f64 },
-    Exponential { lambda: f64 },
-    Laplace { loc: f64, scale: f64 },
+    Exp { lambda: f64 },
+    DoubleExp { lambda1: f64, lambda2: f64, prop_lambda1: f64, cutoff: f64, sample: f64 }
 }
 
 impl Distribution {
@@ -32,13 +72,10 @@ impl Distribution {
                 Normal::new(*mean, *std_dev).unwrap().sample(rng)
             }
             Distribution::Uniform { low, high } => {
-                Uniform::new(*low, *high).sample(rng)
+                Uniform::new(*low, *high).unwrap().sample(rng)
             }
-            Distribution::Exponential { lambda } => {
-                Exponential::new(*lambda).unwrap().sample(rng)
-            }
-            Distribution::Laplace { loc, scale } => {
-                Laplace::new(*loc, *scale).unwrap().sample(rng)
+            Distribution::Exp { lambda } => {
+                Exp::new(*lambda).unwrap().sample(rng)
             }
         }
     }
