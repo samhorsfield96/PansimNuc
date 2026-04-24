ec3c3e7ac734c45f610d49d7fe8084e8d7ddd7ed
Samuel Horsfield <samuel.horsfield@unine.ch>
2026-04-24 17:13:13 +0200

    Adds run parameter sweep with new scripts

--- parameter_sweep/run_parameter_sweep.sh
+++ parameter_sweep/run_parameter_sweep.sh
@@ -11,21 +11,7 @@ sfs_script=../plot_sfs_nuc.R
 mkdir -p $outdir
 
 # specific runs
-# for config in "$config_dir"/testing*.conf; do
-#     run_name=$(basename "$config" .conf)
-#     outdir_run=$outdir/$run_name
-#     mkdir -p "$outdir_run"
-#     echo "Running $run_name..."
-#     # $executable --config "$config" > "$outdir_run/$run_name.log"
-#     # Rscript "$plotting_script" "$outdir_run/root_out.gff" "$outdir_run" --out "$outdir_run/sv_plot.pdf" --width 16 --height 16 --types exon,intron,intergenic,TE-COPY,TE-CUT --link-types exon,TE-COPY,TE-CUT --gap 50000
-#     # Rscript "$DFE_script" "$outdir_run/selection_samples.csv" "$outdir_run/DFE_plot"
-#     # Rscript "$allele_freq_script" "$outdir_run/tracking.csv" "$outdir_run/allele_freq_plot" 3
-#     # Rscript "$TE_script" "$outdir_run" "$outdir_run/TE_copy_numbers"
-#     Rscript "$sfs_script" "$outdir_run" "$outdir_run/sfs_plot"
-# done
-
-#all
-for config in "$config_dir"/*.conf; do
+for config in "$config_dir"/TE-*recombination*.conf; do
     run_name=$(basename "$config" .conf)
     outdir_run=$outdir/$run_name
     mkdir -p "$outdir_run"
@@ -35,8 +21,24 @@ for config in "$config_dir"/*.conf; do
     Rscript "$allele_freq_script" "$outdir_run/tracking.csv" "$outdir_run/allele_freq_plot" 3
     Rscript "$TE_script" "$outdir_run" "$outdir_run/TE_copy_numbers"
     Rscript "$sfs_script" "$outdir_run" "$outdir_run/sfs_plot"
-    #Rscript "$plotting_script" "$outdir_run/root_out.gff" "$outdir_run" --out "$outdir_run/sv_plot.pdf" --width 16 --height 16 --types exon,intron,intergenic,TE-COPY,TE-CUT --link-types exon,TE-COPY,TE-CUT --gap 50000
+    Rscript "$haplotype_script" "$outdir_run/tracking.csv" "$outdir_run/haplotype_analysis" 5
+    Rscript "$plotting_script" "$outdir_run/root_out.gff" "$outdir_run" --out "$outdir_run/sv_plot.pdf" --width 16 --height 16 --types exon,intron,intergenic,TE-COPY,TE-CUT --link-types exon,TE-COPY,TE-CUT --gap 50000
 done
+
+# #all
+# for config in "$config_dir"/*.conf; do
+    # run_name=$(basename "$config" .conf)
+    # outdir_run=$outdir/$run_name
+    # mkdir -p "$outdir_run"
+    # echo "Running $run_name..."
+    # $executable --config "$config" > "$outdir_run/$run_name.log"
+    # Rscript "$DFE_script" "$outdir_run/selection_samples.csv" "$outdir_run/DFE_plot"
+    # Rscript "$allele_freq_script" "$outdir_run/tracking.csv" "$outdir_run/allele_freq_plot" 3
+    # Rscript "$TE_script" "$outdir_run" "$outdir_run/TE_copy_numbers"
+    # Rscript "$sfs_script" "$outdir_run" "$outdir_run/sfs_plot"
+    # Rscript "$haplotype_script" "$outdir_run/tracking.csv" "$outdir_run/haplotype_analysis" 5
+    # Rscript "$plotting_script" "$outdir_run/root_out.gff" "$outdir_run" --out "$outdir_run/sv_plot.pdf" --width 16 --height 16 --types exon,intron,intergenic,TE-COPY,TE-CUT --link-types exon,TE-COPY,TE-CUT --gap 50000
+# done
 
 # #baseline
 # for config in "$config_dir"/baseline*.conf; do
