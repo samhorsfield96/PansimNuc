0093a44270b20d863acb2b52142f7882c9569d50
Samuel Horsfield <samuel.horsfield@unine.ch>
2026-07-03 18:09:47 +0200

    Adds scripts for running param grid for publication

---
+++ parameter_sweep/run_grid_parameter_sweep.sh
@@ -0,0 +1,63 @@
+# mu vs selection vs recombination
+python grid_parameter_sweep.py \
+    --config /home/sam/Software/PansimNuc/publication_configs/mu_vs_selection_vs_recombination_base.conf \
+    --output /home/sam/Software/PansimNuc/parameter_sweep/mu_vs_selection_vs_recombination_grid.csv \
+    --configs-dir /data/sam/analysis/PansimNuc/grid_parameter_sweep/mu_vs_selection_vs_recombination_grid \
+    --outdir-base /scratch/sam_simulations/grid_parameter_sweep/mu_vs_selection_vs_recombination_grid \
+    --param input.gff_file:/home/sam/Software/PansimNuc/testing/Zymo_chr19.gff \
+    --param input.fasta_file:/home/sam/Software/PansimNuc/testing/Zymo_chr19.fasta \
+    --param input.earlgrey_gff_file:/home/sam/Software/PansimNuc/testing/Zymo_EarlGrey_chr19.gff \
+    --param population.n_individuals:50 \
+    --param population.n_generations:500 \
+    --param mutation_rate:1e-12,1e-10,1e-8 \
+    --param selection_distribution:double_exp \
+    --param selection_lambda1:1e14,1e7,1e1 \
+    --param selection_lambda2:1e14,1e7,1e1 \
+    --param selection_cutoff:0.1,0.5,0.9 \
+    --param misc.print_all_generations:true \
+    --param population.recombination_rate:1e-10,1e-8,1e-6
+
+# TE dynamics
+python grid_parameter_sweep.py \
+    --config /home/sam/Software/PansimNuc/publication_configs/mu_vs_selection_vs_recombination_base.conf \
+    --output /home/sam/Software/PansimNuc/parameter_sweep/mu_vs_selection_vs_recombination_vs_TE_grid.csv \
+    --configs-dir /data/sam/analysis/PansimNuc/grid_parameter_sweep/mu_vs_selection_vs_recombination_vs_TE_grid \
+    --outdir-base /scratch/sam_simulations/grid_parameter_sweep/mu_vs_selection_vs_recombination_vs_TE_grid \
+    --param input.gff_file:/home/sam/Software/PansimNuc/testing/Zymo_chr19.gff \
+    --param input.fasta_file:/home/sam/Software/PansimNuc/testing/Zymo_chr19.fasta \
+    --param input.earlgrey_gff_file:/home/sam/Software/PansimNuc/testing/Zymo_EarlGrey_chr19.gff \
+    --param population.n_individuals:50 \
+    --param population.n_generations:500 \
+    --param mutation_rate:1e-10 \
+    --param selection_distribution:double_exp \
+    --param selection_lambda1:1e14,1e1 \
+    --param selection_lambda2:1e14,1e1 \
+    --param selection_cutoff:0.1,0.5,0.9 \
+    --param misc.print_all_generations:false \
+    --param population.recombination_rate:1e-10,1e-8,1e-6 \
+    --param TE-CUT.duplication_rate:1e-30,1e-5,1e-3 \
+    --param TE-CUT.deletion:1e-30,1e-5,1e-3 \
+    --param TE-CUT.duplication_rate:1e-30,1e-5,1e-3 \
+    --param TE-COPY.deletion:1e-30,1e-5,1e-3
+
+# multiple populations
+python grid_parameter_sweep.py \
+    --config /home/sam/Software/PansimNuc/publication_configs/mu_vs_selection_vs_recombination_base.conf \
+    --output /home/sam/Software/PansimNuc/parameter_sweep/mu_vs_selection_vs_recombination_vs_migration_grid.csv \
+    --configs-dir /data/sam/analysis/PansimNuc/grid_parameter_sweep/mu_vs_selection_vs_recombination_vs_migration_grid \
+    --outdir-base /scratch/sam_simulations/grid_parameter_sweep/mu_vs_selection_vs_recombination_vs_migration_grid \
+    --param input.gff_file:/home/sam/Software/PansimNuc/testing/Zymo_chr19.gff \
+    --param input.fasta_file:/home/sam/Software/PansimNuc/testing/Zymo_chr19.fasta \
+    --param input.earlgrey_gff_file:/home/sam/Software/PansimNuc/testing/Zymo_EarlGrey_chr19.gff \
+    --param population.n_individuals:50 \
+    --param population.n_generations:500 \
+    --param mutation_rate:1e-12,1e-10,1e-8 \
+    --param selection_distribution:double_exp \
+    --param selection_lambda1:1e14,1e7,1e1 \
+    --param selection_lambda2:1e14,1e7,1e1 \
+    --param selection_cutoff:0.1,0.5,0.9 \
+    --param misc.print_all_generations:true \
+    --param population.recombination_rate:1e-10,1e-8,1e-6 \
+    --param population.population_splits:2 \
+    --param population.generation_splits:250 \
+    --param population.migration_rate:0.0,0.01,0.1
--- parameter_sweep/run_parameter_sweep.sh
+++ parameter_sweep/run_parameter_sweep.sh
@@ -1,16 +1,12 @@
 executable=../pansimnuc/target/release/PansimNuc
 config_dir=../test_configs
 outdir=/data/sam/analysis/PansimNuc/parameter_sweep
-plotting_script=../sv_plot.R
-allele_freq_script=../plot_allele_frequencies.R
-DFE_script=../print_DFEs.R
-haplotype_script=../plot_haplotypes.R
-TE_script=../plot_te_copy_numbers.R
-sfs_script=../plot_sfs_nuc.R
+SNAKEMAKE_DIR="/home/sam/Software/PansimNucWF"
+SNAKMAKE_CONFIG="/home/sam/Software/PansimNucWF/config/config.yaml"
 
 mkdir -p $outdir
 
-# #baseline
+#baseline
 # for config in "$config_dir"/baseline*.conf; do
 #     run_name=$(basename "$config" .conf)
 #     outdir_run=$outdir/$run_name
@@ -26,7 +22,27 @@ mkdir -p $outdir
 # done
 
 # #specific runs
-# for config in "$config_dir"/exon_mu*.conf; do
+for config in "$config_dir"/baseline_uniform_selection_one_split_gen_25*.conf; do
+    run_name=$(basename "$config" .conf)
+    outdir_run=$outdir/$run_name
+    mkdir -p "$outdir_run"
+    echo "Running $run_name..."
+    #$executable --config "$config" > "$outdir_run/$run_name.log"
+
+    echo "Running $run_name with PansimNucWF..."
+    cd /home/sam/Software/PansimNucWF && pixi run snakemake -s ${SNAKEMAKE_DIR}/Snakefile --directory ${outdir_run} --configfile ${SNAKMAKE_CONFIG} --cores 4 --use-conda --conda-prefix ${SNAKEMAKE_DIR}/.snakemake/conda --config reference=${outdir_run}/root.fasta input_dir=${outdir_run} output_dir=${outdir_run} simulated="True" --unlock
+    cd /home/sam/Software/PansimNucWF && pixi run snakemake -s ${SNAKEMAKE_DIR}/Snakefile --directory ${outdir_run} --configfile ${SNAKMAKE_CONFIG} --cores 4 --use-conda --conda-prefix ${SNAKEMAKE_DIR}/.snakemake/conda --config reference=${outdir_run}/root.fasta input_dir=${outdir_run} output_dir=${outdir_run} simulated="True" > "${outdir_run}/pansimnucWF.log" 2>&1
+
+    #Rscript "$DFE_script" "$outdir_run/selection_samples.csv" "$outdir_run/DFE_plot"
+    #Rscript "$allele_freq_script" "$outdir_run/tracking.csv" "$outdir_run/allele_freq_plot" 3
+    #Rscript "$TE_script" "$outdir_run" "$outdir_run/TE_copy_numbers"
+    #Rscript "$sfs_script" "$outdir_run" "$outdir_run/sfs_plot"
+    #Rscript "$haplotype_script" "$outdir_run/tracking.csv" "$outdir_run/haplotype_analysis" 5
+    #Rscript "$plotting_script" "$outdir_run/root.gff" "$outdir_run" --out "$outdir_run/sv_plot.pdf" --width 16 --height 16 --types exon,intron,intergenic,TE-COPY,TE-CUT --link-types exon,TE-COPY,TE-CUT --gap 50000
+done
+
+#all
+# for config in "$config_dir"/*.conf; do
 #     run_name=$(basename "$config" .conf)
 #     outdir_run=$outdir/$run_name
 #     mkdir -p "$outdir_run"
@@ -37,20 +53,5 @@ mkdir -p $outdir
 #     Rscript "$TE_script" "$outdir_run" "$outdir_run/TE_copy_numbers"
 #     Rscript "$sfs_script" "$outdir_run" "$outdir_run/sfs_plot"
 #     Rscript "$haplotype_script" "$outdir_run/tracking.csv" "$outdir_run/haplotype_analysis" 5
-#     Rscript "$plotting_script" "$outdir_run/root_out.gff" "$outdir_run" --out "$outdir_run/sv_plot.pdf" --width 16 --height 16 --types exon,intron,intergenic,TE-COPY,TE-CUT --link-types exon,TE-COPY,TE-CUT --gap 50000
+#     Rscript "$plotting_script" "$outdir_run/root.gff" "$outdir_run" --out "$outdir_run/sv_plot.pdf" --width 16 --height 16 --types exon,intron,intergenic,TE-COPY,TE-CUT --link-types exon,TE-COPY,TE-CUT --gap 50000
 # done
-
-#all
-for config in "$config_dir"/*.conf; do
-    run_name=$(basename "$config" .conf)
-    outdir_run=$outdir/$run_name
-    mkdir -p "$outdir_run"
-    echo "Running $run_name..."
-    $executable --config "$config" > "$outdir_run/$run_name.log"
-    Rscript "$DFE_script" "$outdir_run/selection_samples.csv" "$outdir_run/DFE_plot"
-    Rscript "$allele_freq_script" "$outdir_run/tracking.csv" "$outdir_run/allele_freq_plot" 3
-    Rscript "$TE_script" "$outdir_run" "$outdir_run/TE_copy_numbers"
-    Rscript "$sfs_script" "$outdir_run" "$outdir_run/sfs_plot"
-    Rscript "$haplotype_script" "$outdir_run/tracking.csv" "$outdir_run/haplotype_analysis" 5
-    Rscript "$plotting_script" "$outdir_run/root_out.gff" "$outdir_run" --out "$outdir_run/sv_plot.pdf" --width 16 --height 16 --types exon,intron,intergenic,TE-COPY,TE-CUT --link-types exon,TE-COPY,TE-CUT --gap 50000
-done
